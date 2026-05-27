e3ea4c897d58b2bbbac7d7f05794ca38d4d220eb
Ryoga-Hasegawa <hasegawaryoume@hasegawaryoumenoMacBook-Air.local>
2026-05-27 18:18:04 +0900

    fix: 重複ファイル・ネストディレクトリの削除

--- CI-CD_study/environments/staging/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "ci-cd-study.tfstate-bucket-812063706542-ap-northeast-1-an"
-    key            = "staging/terraform.tfstate"
-    region         = "ap-northeast-1"
-    dynamodb_table = "ci_cd_study.tfstate"
-    encrypt        = true
-  }
-}
